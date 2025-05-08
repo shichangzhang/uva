4908790d17ab9d07d47395d510d1d53bc0e09505
Chang <changz@live.com.au>
2025-05-09 00:40:41 +1000

    More efficient solution for Problem 12321 without nested while loops (which are slow in python)

--- solutions/12321.py
+++ solutions/12321.py
@@ -12,32 +12,34 @@ while not (l == 0 and g == 0):
     # Greedily pick station which reaches the furthest
     # Sliding window
     left = 0
-    right = 0
-    max_r = 0
+    can_reach = 0
+    max_reach = 0
     result = 0
-    while left < g:
-        best = None
-        while right < g:
-            station = stations[right]
-            if station[0] > max_r:
-                break
-            else:
-                if station[1] > max_r:
-                    if best == None or station[1] > best:
-                        best = station[1]
-            right += 1
-        if best == None:
-            # No solution
+    for right in range(g):
+        # Consider each station
+        station = stations[right]
+        if station[0] > can_reach:
+        # Update can reach and increment result
+            if max_reach > can_reach:
+                result += 1
+                can_reach = max_reach
+        
+        # If left is within reach then update max reach
+        if station[0] <= can_reach:
+            max_reach = max(max_reach, station[1])
+        # If max reach is >= L then break, found solution
+        if max_reach >= l:
             break
-        if max_r >= l:
-            # Found solution
+        # If max reach < left then break, no solution
+        if max_reach < station[0]:
             break
-
-        max_r = best
+    
+    # if max reach > can reach then increment result one last time
+    if max_reach > can_reach:
         result += 1
-        left = right
+        can_reach = max_reach
 
-    if max_r >= l:
+    if max_reach >= l:
         print(g-result)
     else:
         print(-1)
